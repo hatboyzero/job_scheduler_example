68b07e5a4ba2319c7f45a9a0c0f2caee4f126ca2
Matthew Alan Gray <mgray@hatboysoftware.com>
2018-04-09 17:45:49 -0500

    Testing job definition containing single job

--- lib/job_scheduler.rb
+++ lib/job_scheduler.rb
@@ -1,5 +1,10 @@
 class JobScheduler
   def execute(job_definition)
-    job_definition
+    output = ''
+    job_definition.each_line do |line|
+      output << line[0,1]
+    end
+
+    output
   end
 end
--- spec/job_scheduler_spec.rb
+++ spec/job_scheduler_spec.rb
@@ -6,4 +6,11 @@ RSpec.describe JobScheduler, '#execute' do
     output = job_scheduler.execute ''
     expect(output.is_a?(String) && output.empty?)
   end
+
+  it 'executes a job definition containing a single job' do
+    job_scheduler = JobScheduler.new
+    output = job_scheduler.execute 'a => '
+    print output
+    expect(output).to eq('a')
+  end
 end
