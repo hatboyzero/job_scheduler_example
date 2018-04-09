7010eb575d8484400005a44b76f0a6a9984ef428
Matthew Alan Gray <mgray@hatboysoftware.com>
2018-04-09 18:29:10 -0500

    Testing a job definition containing multiple jobs with dependencies

--- lib/job_scheduler.rb
+++ lib/job_scheduler.rb
@@ -1,8 +1,21 @@
 class JobScheduler
   def execute(job_definition)
+    jobs = job_definition.each_line.map do |line|
+      name, dep = line.split('=>').map { |v| v.strip! }
+      { name => dep }
+    end
+
     output = ''
-    job_definition.each_line do |line|
-      output << line[0,1]
+    while jobs.any?
+      current_job = jobs.shift(1).first
+      job_name = current_job.keys.first.to_s
+      job_dep = current_job[job_name]
+      unless job_dep.empty?
+        output << job_dep
+        jobs.delete_if { |job| job.keys.first.to_s == job_dep }
+      end
+
+      output << job_name
     end
 
     output
--- spec/job_scheduler_spec.rb
+++ spec/job_scheduler_spec.rb
@@ -1,25 +1,39 @@
 require 'job_scheduler'
 
 RSpec.describe JobScheduler, '#execute' do
+  before(:each) do
+    @job_scheduler = JobScheduler.new
+  end
+
   it 'executes an empty job definition' do
-    job_scheduler = JobScheduler.new
-    output = job_scheduler.execute ''
-    expect(output.is_a?(String) && output.empty?)
+    output = @job_scheduler.execute ''
+    expect(output).to be_a(String)
+    expect(output).to be_empty
   end
 
   it 'executes a job definition containing a single job' do
-    job_scheduler = JobScheduler.new
-    output = job_scheduler.execute 'a => '
+    output = @job_scheduler.execute 'a => '
+    expect(output).to be_a(String)
     expect(output).to eq('a')
   end
 
   it 'executes a job definition containing multiple jobs' do
-    job_scheduler = JobScheduler.new
-    output = job_scheduler.execute <<EOM
+    output = @job_scheduler.execute <<EOM
 a =>
 b =>
 c =>
 EOM
+    expect(output).to be_a(String)
     expect(output).to eq('abc')
+  end
+
+  it 'executes a job definition containing multiple jobs with dependencies' do
+    output = @job_scheduler.execute <<EOM
+a => 
+b => c
+c =>
+EOM
+    expect(output).to be_a(String)
+    expect(output).to eq('acb')
   end
 end
